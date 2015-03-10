38ba00e98005a8aed27e15e421dadaebcffb6168
Allen Gooch <allen.gooch@gmail.com>
2015-03-10 13:22:17 -0500

    changed main page to only display tastings occuring within the next 7 days

--- client/templates/tastings/tastings_list.js
+++ client/templates/tastings/tastings_list.js
@@ -1,14 +1,14 @@
 Template.tastingsList.helpers({
 
   tastings: function () {
-    var begin = moment().day(0).hour(0).minute(0).seconds(0).toDate();
-    var end = moment().day(6).hour(23).minutes(59).seconds(59).toDate();
+    var begin = moment().toDate();
+    var end = moment().add(7, 'days').toDate();
     return Tastings.find({when: {'$gte': begin, '$lte': end}}, {sort: {when: 1}});
   },
 
   noTastings: function () {
-    var begin = moment().day(0).hour(0).minute(0).seconds(0).toDate();
-    var end = moment().day(6).hour(23).minutes(59).seconds(59).toDate();
+    var begin = moment().toDate();
+    var end = moment().add(7, 'days').toDate();
     return Tastings.find({when: {'$gte': begin, '$lte': end}}, {sort: {when: 1}}).count() === 0;
   }
 
