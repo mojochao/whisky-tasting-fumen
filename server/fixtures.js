66463c4c51108ed97cb319bf7d620085b1ed4560
Allen Gooch <allen.gooch@gmail.com>
2015-03-30 16:52:26 -0500

    added demo users and tastings data

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,31 +1,90 @@
 // create some test users
 if (Meteor.users.find().count() === 0) {
-  var userIdAllen = Users.createUser({
-    username: 'allen',
-    password: 'password'
+  var useridAllen = Meteor.users.insert({
+    username: 'allen'
   });
-  var userIdBryan = Users.createUser({
-    username: 'bryan',
-    password: 'password'
+  Accounts.setPassword(useridAllen, 'password')
+  var userAllen = Meteor.users.findOne(useridAllen);
+
+  var useridBryan = Meteor.users.insert({
+    username: 'bryan'
   });
-  var userIdCatherine = Users.createUser({
-    username: 'catherine',
-    password: 'password'
+  Accounts.setPassword(useridBryan, 'password')
+  var userBryan = Meteor.users.findOne(useridBryan);
+
+  var useridCatherine = Meteor.users.insert({
+    username: 'catherine'
   });
-  var userIdDanielle = Users.createUser({
-    username: 'danielle',
-    password: 'password'
-  });
+  Accounts.setPassword(useridCatherine, 'password')
+  var userCatherine = Meteor.users.findOne(useridCatherine);
 }
 
 // create some test tastings
-if (Meteor.tastings.find().count() === 0) {
-  var pastTasting1 = Tastings.create({
-    _id = userIdAllen,
-    when: '',
+if (Tastings.find().count() === 0) {
+  var pastTasting1 = Tastings.insert({
+    userId: useridAllen,
+    host: userAllen.username,
+    submitted: new Date("2015-03-24T23:00:00Z"),
+    commentsCount: 0,
+    when: new Date("2015-03-25T23:00:00Z"),
     where: 'Heroes & Legacies',
     bottle: 'Pyrat Reserve',
-    spirit: 'Rum'
+    spirit: 'Rum',
     teaser: 'Cuz pirates, yo!'
-  })
+  });
+  var pastTasting2 = Tastings.insert({
+    _id: useridBryan,
+    host: userBryan.username,
+    submitted: new Date("2015-03-17T23:00:00Z"),
+    commentsCount: 0,
+    when: new Date("2015-03-18T23:00:00Z"),
+    where: 'Heroes & Legacies',
+    bottle: 'Grey Goose',
+    spirit: 'Vodka',
+    teaser: 'Clean and clear'
+  });
+  var nextTasting1 = Tastings.insert({
+    userId: useridAllen,
+    host: userAllen.username,
+    submitted: new Date("2015-03-31T23:00:00Z"),
+    commentsCount: 0,
+    when: new Date("2015-04-01T23:00:00Z"),
+    where: 'Heroes & Legacies',
+    bottle: 'Willet Reserve',
+    spirit: 'Bourbon',
+    teaser: 'In addition to the rye...'
+  });
+  var nextTasting2 = Tastings.insert({
+    userId: useridCatherine,
+    host: userCatherine.username,
+    submitted: new Date("2015-03-31T23:00:01Z"),
+    commentsCount: 0,
+    when: new Date("2015-04-01T23:00:00Z"),
+    where: 'Heroes & Legacies',
+    bottle: 'Willet Reserve 12 Year',
+    spirit: 'Rye',
+    teaser: 'Better than bourbon'
+  });
+  var futureTasting1 = Tastings.insert({
+    userId: useridBryan,
+    host: userBryan.username,
+    submitted: new Date("2015-04-07T23:00:00Z"),
+    commentsCount: 0,
+    when: new Date("2015-04-08T23:00:00Z"),
+    where: 'Heroes & Legacies',
+    bottle: 'Woodford',
+    spirit: 'Bourbon',
+    teaser: 'Not too shabby'
+  });
+  var futureTasting2 = Tastings.insert({
+    userId: useridBryan,
+    host: userBryan.username,
+    submitted: new Date("2015-04-07T23:01:00Z"),
+    commentsCount: 0,
+    when: new Date("2015-04-08T23:00:00Z"),
+    where: 'Heroes & Legacies',
+    bottle: 'Woodford Reserve',
+    spirit: 'Bourbon',
+    teaser: 'Better than shabby'
+  });
 }
