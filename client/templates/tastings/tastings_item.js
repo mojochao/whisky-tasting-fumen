c665213b67eb079fc6fcbca755ac46919ecddf33
Allen Gooch <allen.gooch@gmail.com>
2015-03-31 13:18:28 -0500

    added support for displaying existing tasting ratings.

--- client/templates/tastings/tastings_item.js
+++ client/templates/tastings/tastings_item.js
@@ -4,6 +4,13 @@ Template.tastingsItem.helpers({
 
 });
 
+Template.tastingsItem.rendered = function () {
+  // at .created() time, it's too early to run rateit(), so run it
+  // at .rendered() time.
+  this.$('.rateit').rateit();
+}
+
+
 Template.tastingsItem.events = {
 
   'click .tastings-item-select': function () {
