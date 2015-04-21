a9cd3fdba9d36c5fb6bc670acb66c871edc5ace9
Allen Gooch <allen.gooch@gmail.com>
2015-04-21 14:52:56 -0500

    finished refactoring of template files to match template names.

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -9,27 +9,3 @@ Template.notifications.helpers({
   }
 
 });
-
-Template.notificationItem.helpers({
-
-  notificationTastingPath: function () {
-    return Router.routes.tastingPage.path({_id: this.tastingId});
-  },
-
-  notificationContent: function () {
-    if (this.notificationType === "comment") {
-      return "commented on your tasting"
-    } else if (this.notificationType === "rating"){
-      return "rated your tasting"
-    }
-  }
-
-});
-
-Template.notificationItem.events({
-
-  'click a': function () {
-    Notifications.update(this._id, {$set: {read: true}});
-  }
-
-});
