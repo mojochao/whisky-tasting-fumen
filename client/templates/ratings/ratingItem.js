940a4229b1269bdf5c4bb8577657b23e7accf322
Allen Gooch <allen.gooch@gmail.com>
2015-04-23 10:26:16 -0500

    Eased constraints on editing, commenting, and rating tastings. Fixed display issue on newly submitted ratings.

--- client/templates/ratings/ratingItem.js
+++ client/templates/ratings/ratingItem.js
@@ -5,3 +5,9 @@ Template.ratingItem.helpers({
   }
 
 });
+
+Template.ratingItem.rendered = function () {
+  // at .created() time, it's too early to run rateit(), so run it
+  // at .rendered() time.
+  this.$('.rateit').rateit();
+};
